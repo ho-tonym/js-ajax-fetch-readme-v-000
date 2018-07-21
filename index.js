53af899691f6c33c09b4736da48f9440c1216ed7
ho-tonym <ho.tonym@gmail.com>
2018-07-21 15:53:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+const token = '750d0a5ed8993c1d82b415f56ddc95f00d17b577'
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json)) 
